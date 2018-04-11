9d647cf4e4a28f6abefe6f7159a03982546488b0
Desm0nt <logran2@gmail.com>
2018-04-11 14:15:27 +0300

    init

---
+++ StringTask/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> text = new List<string>();
+            text.Add("Quick brown fox jump over tha lazy dog. 123");
+            text.Add(null);
+            text.Add("The End.");
+            text = TextTransformer.TextToUpperCase(text);
+            foreach (var a in text)
+            {
+                Console.WriteLine(a);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ StringTask/TextTransformer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTask
+{
+    class TextTransformer
+    {
+
+        public static List<string> TextToUpperCase(List<string> txt)
+        {
+            for (int i = 0; i< txt.Count; i++)
+            {
+                if (string.IsNullOrEmpty(txt[i]))
+                {
+                    txt[i] = "_";
+                }
+                else
+                {
+                    txt[i] = new string(ToUpperCase(txt[i]).ToArray());
+                }
+            }
+            return txt;
+        }
+
+        static IEnumerable<char> ToUpperCase(string s)
+        {
+            foreach (char c in s)
+            {
+                yield return Char.ToUpper(c);
+            }
+        }
+    }
+}
